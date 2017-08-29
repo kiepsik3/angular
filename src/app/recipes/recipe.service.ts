7e0491ac404a390b915175bbf414d7e6a590c62c
Damian <damianos68@gmail.com>
2017-08-29 23:23:32 +0200

    Rozdział 17

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,16 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from "rxjs/Subject";
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 
+
 @Injectable()
 
 export class RecipeService {
-recipeSelected = new EventEmitter<Recipe>();
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
     new Recipe ('Test', 'Zobaczmy czy działa', 'http://www.zajadam.pl/wp-content/uploads/hamburger-przepis-469x313.jpg', [
@@ -29,7 +31,26 @@ recipeSelected = new EventEmitter<Recipe>();
       return this.recipes.slice();
   }
 
+  getRecipe(index: number) {
+      return this.recipes[index];
+  }
+
   addIngredientToShoppingList(ingredient: Ingredient[]) {
     this.slService.addIngredients(ingredient);
+  }
+
+  addRecipe(recipe: Recipe) {
+      this.recipes.push(recipe);
+      this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+      this.recipes[index] = newRecipe;
+      this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+      this.recipes.splice(index, 1);
+      this.recipesChanged.next(this.recipes.slice());
   }
 }
